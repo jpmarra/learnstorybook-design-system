fb318195656dc0a2233223e962109db7ccf9fcac
jmarra <jp.marra@mlb.com>
2019-10-07 15:23:03 -0400

    adds documentation steps

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,22 @@
-import React from "react";
-import { configure, addDecorator } from "@storybook/react";
-import { GlobalStyle } from "../src/components/shared/global";
-import "storybook-chromatic";
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import { GlobalStyle } from '../src/components/shared/global'
+import { withA11y } from '@storybook/addon-a11y'
+import 'storybook-chromatic'
 
+addDecorator(withA11y)
 addDecorator(story => (
-  <>
-    <GlobalStyle />
-    {story()}
-  </>
-));
+    <>
+        <GlobalStyle />
+        {story()}
+    </>
+))
 
-// automatically import all files ending in *.stories.js
-configure(require.context("../src", true, /\.stories\.js$/), module);
+// automatically import all files ending in *.stories.js|mdx
+configure(
+    [
+        require.context('../src', true, /Intro\.stories\.mdx/),
+        require.context('../src', true, /\.stories\.(js|mdx)$/),
+    ],
+    module
+)
